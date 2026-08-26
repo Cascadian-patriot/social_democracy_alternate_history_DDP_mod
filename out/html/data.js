1e6b0f8c8fdec1c4df71ed22a29008e01f07d57b
socialdemocracy <haeltor80@gmail.com>
2026-08-26 12:43:39 -0600

    fewafewafewa

---
+++ out/html/data.js
@@ -0,0 +1,97 @@
+const colourList = [{
+        word: "KPD",
+        style: "color: #8B0000; font-weight: bold;"
+   },
+    {
+        word: "SPD",
+        style: "color: #E3000F; font-weight: bold;"
+  },
+    {
+        word: "DDP",
+        style: "color: #DCCA4A; font-weight: bold;"
+   },
+    {
+        word: "Z",
+        style: "color: #000; font-weight: bold;"
+   },
+    {
+        word: "BVP",
+        style: "color: #69A2BE; font-weight: bold;"
+   },
+    {
+        word: "DNVP",
+        style: "color: #3F7BC1; font-weight: bold;"
+   },
+    {
+        word: "NSDAP",
+        style: "color: #954B00; font-weight: bold;"
+   },
+    {
+        word: "SAPD",
+        style: "color: #C40000; font-weight: bold;"
+   },
+    {
+        word: "Others",
+        style: "color: #909090; font-weight: bold;"
+    },
+];
+
+
+
+const tooltipList = [{
+    searchString: "KPD",
+    explanationText: "<img src=img/kpd(2).svg> Kommunistische Partei Deutschlands </br></br> Communist Party Of Germany"
+    },
+    {
+    searchString: "PSI",
+    explanationText: "<img src=img/psi-Photoroom-removebg-preview.png> Partito Socialista Italiano </br></br> Italian Socialist Party"
+    },
+    {
+    searchString: "SPD",
+    explanationText: "<img src=img/spd-Photoroom.png> Sozialdemokratische Partei Deutschlands </br> Social Democratic Party Of Germany"
+    },
+    {
+    searchString: "Communist",
+    explanationText: "<img src=img/PreparingRevolution.jpg> The Communists are interested in radical, transformative policies and revolutionary action."
+    },
+    {
+    searchString: "Maximalist",
+    explanationText: "<img src=img/ShuffleLeadership.jpg> The Maximalists are a faction mostly comprised of Center-Marxists, between reform and revolution."
+    },
+    {
+    searchString: "Labour",
+    explanationText: "<img src=img/TurinFactories.jpg> Labour's goal is economic reform and benefits for organized workers."
+    },
+    {
+    searchString: "Reformist",
+    explanationText: "<img src=img/DemBg.jpg> Reformists are pragmatic participants in government, and seek to expand the welfare state and defend democracy."
+    },
+    {
+    searchString: "Syndicalist",
+    explanationText: "<img src=img/ItalianSyndicalistUnion.jpg> Syndicalist's are a splinter faction of Labour, advocating for radical strikes and workers' councils."
+    },
+    {
+    searchString: "PCd'I",
+    explanationText: "<img src=img/pcdi.webp> Partito Comunista Italiano </br></br> Italian Communist Party"
+    },
+    {
+    searchString: "pcdi",
+    explanationText: "<img src=img/pcdi.webp> Partito Comunista Italiano </br></br> Italian Communist Party"
+    },
+    {
+    searchString: "PSU",
+    explanationText: "<img src=img/psu.webp> Partito Socialista Unitario </br></br> Unitary Socialist Party"
+    },
+    {
+    searchString: "Others",
+    explanationText: "<img src=img/partismaller-Photoroom.png> Several smaller parties in the Italian political landscape."
+    },
+    {
+    searchString: "PLL",
+    explanationText: "<img src=img/pll_logo_2.png> Partito Liberale dei Lavoratori </br></br> Liberal Worker's Party"
+    },
+    {
+    searchString: "PRI",
+    explanationText: "<img src=img/pribetter.webp>  Partito Republicano Italiano </br></br> Italian Republican Party"
+    },
+];
