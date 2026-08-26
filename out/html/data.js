f0579552379120020f5fd72d3b98c8ef81938de9
socialdemocracy <haeltor80@gmail.com>
2026-08-26 13:33:23 -0600

    fewafewafewaf

--- out/html/data.js
+++ out/html/data.js
@@ -72,7 +72,7 @@ const tooltipList = [{
     },
     {
     searchString: "DNVP",
-    explanationText: "<img src=img/dnvp-Photoroom-removebg-preview (1).png> Deutschnationale Volkspartei </br></br> German National People's Party"
+    explanationText: "<img src=img/dnvp_good_one.png> Deutschnationale Volkspartei </br></br> German National People's Party"
     },
     {
     searchString: "NSDAP",
